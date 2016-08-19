b85f9c54ae8d130590bd2bec84f58793c81db5c5
Camron Khan <camron.khan@gmail.com>
2016-08-19 16:34:05 -0500

    updated logout route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
 
 	root 'static_pages#home'
-  
+   
 	get     'home',       	to: 'static_pages#home',        as: 'home'
 	get     'about',      	to: 'static_pages#about',       as: 'about'
 	get     'education',  	to: 'static_pages#education',   as: 'education'
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 	get     'contact',    	to: 'static_pages#contact', 	as: 'contact'
 	get		'login',		to: 'sessions#new',				as: 'login'
 	post	'login',		to: 'sessions#create',
-	get		'logout',		to: 'sessions#destroy',			as: 'logout'
+	delete	'logout',		to: 'sessions#destroy'
 
 	resources :projects, only: [:index, :show]
 end
